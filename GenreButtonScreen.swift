266f2b24b30456bbd8d16b5ae65a7cceb15f72c6
Igor <rozumiq@gmail.com>
2019-10-14 18:18:13 -0400

    added

---
+++ GenreButtonScreen.swift
@@ -0,0 +1,50 @@
+//
+//  GenreButtonScreen.swift
+//  MusicApp
+//
+//  Created by Igor Rozum on 2019-08-07.
+//  Copyright © 2019 Igor Rozum. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func GenreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String) {
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+    
+}
